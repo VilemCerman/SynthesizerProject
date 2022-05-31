d59bf3474ea353e89967b70b9a410d8b5038eb3d
Vilem <vilem.cerman@seznam.cz>
2022-05-31 09:13:41 +0200

    First commit

---
+++ Basic Synth/BasicSynth.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Basic_Synth
+{
+    partial class BasicSynth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oscillator5 = new Basic_Synth.Oscillator();
+            this.oscillator4 = new Basic_Synth.Oscillator();
+            this.oscillator3 = new Basic_Synth.Oscillator();
+            this.oscillator2 = new Basic_Synth.Oscillator();
+            this.oscillator1 = new Basic_Synth.Oscillator();
+            this.SuspendLayout();
+            // 
+            // oscillator5
+            // 
+            this.oscillator5.BackColor = System.Drawing.Color.Purple;
+            this.oscillator5.Location = new System.Drawing.Point(283, 13);
+            this.oscillator5.Name = "oscillator5";
+            this.oscillator5.Size = new System.Drawing.Size(264, 100);
+            this.oscillator5.TabIndex = 8;
+            this.oscillator5.TabStop = false;
+            this.oscillator5.Text = "oscillator5";
+            // 
+            // oscillator4
+            // 
+            this.oscillator4.Location = new System.Drawing.Point(13, 331);
+            this.oscillator4.Name = "oscillator4";
+            this.oscillator4.Size = new System.Drawing.Size(264, 100);
+            this.oscillator4.TabIndex = 8;
+            this.oscillator4.TabStop = false;
+            this.oscillator4.Text = "oscillator4";
+            // 
+            // oscillator3
+            // 
+            this.oscillator3.Location = new System.Drawing.Point(12, 225);
+            this.oscillator3.Name = "oscillator3";
+            this.oscillator3.Size = new System.Drawing.Size(264, 100);
+            this.oscillator3.TabIndex = 8;
+            this.oscillator3.TabStop = false;
+            this.oscillator3.Text = "oscillator3";
+            // 
+            // oscillator2
+            // 
+            this.oscillator2.Location = new System.Drawing.Point(13, 119);
+            this.oscillator2.Name = "oscillator2";
+            this.oscillator2.Size = new System.Drawing.Size(264, 100);
+            this.oscillator2.TabIndex = 7;
+            this.oscillator2.TabStop = false;
+            this.oscillator2.Text = "oscillator2";
+            // 
+            // oscillator1
+            // 
+            this.oscillator1.Location = new System.Drawing.Point(13, 13);
+            this.oscillator1.Name = "oscillator1";
+            this.oscillator1.Size = new System.Drawing.Size(264, 100);
+            this.oscillator1.TabIndex = 0;
+            this.oscillator1.TabStop = false;
+            this.oscillator1.Text = "oscillator1";
+            // 
+            // BasicSynth
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(607, 486);
+            this.Controls.Add(this.oscillator5);
+            this.Controls.Add(this.oscillator4);
+            this.Controls.Add(this.oscillator3);
+            this.Controls.Add(this.oscillator2);
+            this.Controls.Add(this.oscillator1);
+            this.KeyPreview = true;
+            this.Name = "BasicSynth";
+            this.Text = "Synth";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BasicSynth_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Oscillator oscillator1;
+        private Oscillator oscillator2;
+        private Oscillator oscillator3;
+        private Oscillator oscillator4;
+        private Oscillator oscillator5;
+    }
+}
+
---
+++ Basic Synth/Oscillator.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Synth
+{
+    class Oscillator : GroupBox
+    {
+        public Oscillator()
+        {
+            this.BackColor = Color.Purple;
+            this.Controls.Add(new Button()
+            {
+                Name = "Sine",
+                Location = new Point(10, 15),
+                Text = "Sine",
+                BackColor = Color.MediumPurple
+            });
+            this.Controls.Add(new Button()
+            {
+                Name = "Square",
+                Location = new Point(65, 15),
+                Text = "Square",
+                UseVisualStyleBackColor = true
+        });
+            this.Controls.Add(new Button()
+            {
+                Name = "Saw",
+                Location = new Point(120, 15),
+                Text = "Saw",
+                UseVisualStyleBackColor = true
+            });
+            this.Controls.Add(new Button()
+            {
+                Name = "Tringle",
+                Location = new Point(10, 50),
+                Text = "Triangle",
+                UseVisualStyleBackColor = true
+            });
+            this.Controls.Add(new Button()
+            {
+                Name = "Noise",
+                Location = new Point(65, 50),
+                Text = "Noise",
+                UseVisualStyleBackColor = true
+
+            });
+            //this.Controls.Add(new TextBox()
+            //{
+            //    Name = "Pitch",
+            //    Location = new Point(120, 50),
+            //    Text = "1"
+            //});
+
+            TrackBar PitchShift_TrB = new TrackBar();
+            PitchShift_TrB.Name = "PitchShift";
+            
+            PitchShift_TrB.Location = new Point(175, 8);
+            PitchShift_TrB.Size = new Size(56, 90);
+            PitchShift_TrB.Orientation = Orientation.Vertical;
+            PitchShift_TrB.TickStyle = TickStyle.TopLeft;
+            PitchShift_TrB.Maximum = 12;
+            PitchShift_TrB.Minimum = -12;
+            PitchShift_TrB.TickFrequency = 2;
+            PitchShift_TrB.Scroll += PitchShiftTrB_Scroll;
+            PitchShift_TrB.Parent = this;
+
+            TrackBar Volume_TrB = new TrackBar();
+            Volume_TrB.Name = "Volume";
+            Volume_TrB.Location = new Point(220, 8);
+            Volume_TrB.Size = new Size(56, 90);
+            Volume_TrB.Orientation = Orientation.Vertical;
+            Volume_TrB.TickStyle = TickStyle.TopLeft;
+            Volume_TrB.Maximum = 10;
+            Volume_TrB.Minimum = 0;
+            Volume_TrB.TickFrequency = 1;
+            Volume_TrB.Value = 10;
+            Volume_TrB.Scroll += VolumeTrB_Scroll;
+            Volume_TrB.Parent = this;
+
+            foreach (Control control in this.Controls.OfType<Button>())
+            {
+                control.Size = new Size(50, 30);
+                control.Font = new Font("Microsoft Sans Serif", 6.5f);
+                control.Click += WaveButton_Click;
+            }
+
+            
+            //foreach (Control control in this.Controls.OfType<TextBox>())
+            //{
+            //    control.Size = new Size(50, 30);
+            //    control.Font = new Font("Microsoft Sans Serif", 6.5f);
+            //    control.KeyDown += PitchTb_KeyDown;
+            //}
+
+        }
+
+        public WaveForm WaveForm { get; private set; }
+        public int PitchShift { get; private set; }
+        public double Volume { get; private set; } = 1;
+
+        private void WaveButton_Click(object sender, EventArgs e)
+        {
+            if(sender is Button)
+            {
+                Button button = (Button)sender;
+                this.WaveForm = (WaveForm)Enum.Parse(typeof(WaveForm), button.Text);
+                foreach(Button otherButtons in this.Controls.OfType<Button>())
+                {
+                    otherButtons.UseVisualStyleBackColor = true;
+                }
+                button.BackColor = Color.MediumPurple;
+            }
+        }
+
+        private void PitchShiftTrB_Scroll(object sender, EventArgs e)
+        {
+            TrackBar TrB = (TrackBar)sender;
+            PitchShift = TrB.Value;
+        }
+
+        private void VolumeTrB_Scroll(object sender, EventArgs e)
+        {
+            TrackBar TrB = (TrackBar)sender;
+            Volume = TrB.Value / (double)10;
+        }
+
+        //private void PitchTb_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    TextBox textBox = (TextBox)sender;
+        //    if (int.TryParse(textBox.Text, out int result) && Convert.ToInt32(textBox.Text) >= -12 && Convert.ToInt32(textBox.Text) <= 12)
+        //    {
+        //        PitchShift = result;
+        //    }
+        //    if (e.KeyCode == Keys.Enter)
+        //    {
+        //        foreach (TextBox TB in this.Controls.OfType<TextBox>())
+        //        {
+
+        //        }
+        //    }
+        //    //this.Controls.OfType<TextBox>() = null;
+        //    //this.ActiveControl = null;
+        //}
+    }
+}
